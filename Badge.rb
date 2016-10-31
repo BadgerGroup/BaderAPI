14caf2a9b13b235e91ba3a8c50243f4d92ec54ab
kvndmra <kdemoura121@gmail.com>
2016-10-31 12:02:03 -0400

    added validation for badge input

--- Badge.rb
+++ Badge.rb
@@ -3,6 +3,11 @@ class Badge < ActiveRecord::Base
   belongs_to :author, :class_name => 'User', foreign_key: 'author_id'
   belongs_to :recipient, :class_name => 'User', foreign_key: 'recipient_id'
   
+  validates :badge_name, presence: true
+  validates :image_url, presence: true
+  validates :badge_description, presence: true
+  validates :author_id, presence: true
+  
   # returns array representation of user
   def toArray
     {
